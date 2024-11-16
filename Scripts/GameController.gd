f767e0008a315fe58ed19ac79d397f649dba6ed6
Richard Sun <sunr5@rpi.edu>
2024-11-16 17:52:11 -0500

    Create dialogue manager

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -1,12 +1,15 @@
 extends Node
 
 @onready var clock = $"Clock Sprite"
+@onready var dialogue_manager = $DialogueManager
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	if not has_node("DialogueManager"):
+		var dialogue_manager_scene = preload("res://scenes/DialogueManager.tscn")
+		var dialogue_manager_instance = dialogue_manager_scene.instantiate()
+		add_child(dialogue_manager_instance)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
