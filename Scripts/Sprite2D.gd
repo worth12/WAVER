4ccfac5d342dd6186d323e5f5d8cf6f2d054fb8f
Richard Sun <sunr5@rpi.edu>
2024-11-16 20:09:21 -0500

    fix: prevent sprite clicking when a dialogue timeline is running

--- Scripts/Sprite2D.gd
+++ Scripts/Sprite2D.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 
 # Signal to detect mouse input
 func _input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and Dialogic.current_timeline == null:
 		if get_global_mouse_position().distance_to(global_position) <= texture.get_size().x / 2:
 			_on_sprite_clicked()
 
